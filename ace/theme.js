5200ac9adeb65e43c624f755ed2f7f6f05656655
Aaron O'Mullan <aaron.omullan@friendco.de>
2014-03-28 22:54:34 -0700

    Switch theme from less to css

--- ace/theme.js
+++ ace/theme.js
@@ -1,10 +1,10 @@
 define([
-    "less!ace/theme.less"
+    "text!ace/theme.css"
 ], function(cssContent) {
 
     return {
         'isDark': true,
         'cssClass': "ace-monokai",
         'cssText': cssContent
-    }
+    };
 });
